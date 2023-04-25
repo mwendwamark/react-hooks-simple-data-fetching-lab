e4624d687b8a9b8b18f2141f0c1114f0ac42e452
mark mwendwa <mwendwamark310@gmail.com>
2023-04-25 16:28:59 +0300

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,30 @@
-// create your App component here
+import React, {useEffect, useState} from "react";
+
+function App()  {
+    const [image, setImage] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+ useEffect(() =>{
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then(response => response.json())
+    .then (data =>{
+        setImage(data.message);
+        setLoading(false);
+    });
+
+ }, []);
+
+ if (loading) {
+    return (
+    <p>Loading</p>)
+ }
+
+
+    return ( 
+        <div>
+            <img src={image} alt="A Random Dog" />
+        </div>
+     );
+}
+ 
+export default App;
